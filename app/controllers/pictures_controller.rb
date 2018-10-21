9134fb871bd796785b7780c1d9f10b4856eb2542
Daniel Alvarez <dan.alvarez7@gmail.com>
2018-10-21 11:34:59 +0200

    authenticate user before giving him access to different actions

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PicturesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_ownership
   before_action :load_picture, only: [:edit, :update, :show]
 
   def index
@@ -26,6 +28,14 @@ class PicturesController < ApplicationController
 
   def load_picture
     @picture = Picture.find_by_id(params[:id])
+  end
+
+  def ensure_ownership
+    redirect_to root_path unless authorized_user?
+  end
+
+  def authorized_user?
+    current_user && current_user.id == params[:user_id].to_i
   end
 
   def picture_params
