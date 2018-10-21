10959566c7490d174373edfa6039672fee6f4416
Daniel Alvarez <dan.alvarez7@gmail.com>
2018-10-21 11:43:01 +0200

    set basic_authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  if Rails.env.production?
+    http_basic_authenticate_with name: Rails.application.credentials[:username_key],
+                                 password: Rails.application.credentials[:password_key]
+  end
 end
